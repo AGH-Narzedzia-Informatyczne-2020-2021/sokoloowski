632b6f2c40d6f47d7b5839c60be9d32afb66b3af
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-11-03 19:35:10 +0100

    Empty Arduino sketch

---
+++ Arduino sketches/BeeBoard XM 0.3/main/main.ino
@@ -0,0 +1,20 @@
+/**
+ * @file test.ino
+ * @author Piotr Sokołowski (psokolowski@student.agh.edu.pl)
+ * @brief Test file for BeeBoard XM 0.3
+ * @version 0.1
+ * @date 2020-11-03
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+void setup()
+{
+    // code...
+}
+
+void loop()
+{
+    // code...
+}
