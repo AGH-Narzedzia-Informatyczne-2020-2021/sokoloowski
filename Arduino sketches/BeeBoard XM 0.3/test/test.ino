99846534fcccad5e27948c648a9884778cab8244
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-11-05 19:36:56 +0100

    Sending data over ESP-NOW

--- Arduino sketches/BeeBoard XM 0.3/test/test.ino
+++ Arduino sketches/BeeBoard XM 0.3/test/test.ino
@@ -15,6 +15,9 @@
 #include "defines.hpp"
 #include "logstruct.hpp"
 
+// Set up broadcast address to AA:BB:CC:DD:EE:FF
+uint8_t broadcastAddress[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
+
 logstruct beeboard;
 
 void setup()
@@ -23,6 +26,7 @@ void setup()
     digitalWrite(LED, HIGH); // Turn LED off
     Wire.begin();            // Enable I²C interface
     Serial.begin(115200);    // Enable serial interface at baudrate 115200
+    enableEspNow(BroadcastAddress);
     // Serial.println();        // After boot there are some chars, so let's go to new line before test
     // i2c_scan();              // Submit scan
     // Post-setup message
@@ -32,5 +36,9 @@ void setup()
 
 void loop()
 {
+    beeboard.adcValue = readADC(ADC);
+    beeboard.voltage = readVoltage(ADC, MAX_VOLTAGE, MAX_ADC_VALUE);
+    beeboard.messageCode = checkBattery();
+    esp_now_send(0, (uint8_t *)&beeboard, sizeof(beeboard));
     delay(1000);      // Wait for one second
 }
