a015a49d1d7b773681a2f3e57f289b4b1b1a433c
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-11-05 19:31:34 +0100

    Returning int value instead of printing info

--- Arduino sketches/BeeBoard XM 0.3/test/pwrmgmt.ino
+++ Arduino sketches/BeeBoard XM 0.3/test/pwrmgmt.ino
@@ -10,22 +10,25 @@
  */
 
 /**
- * @brief Check voltage and apply specified settings
+ * @brief Check voltage and return info code
  */
-void checkBattery()
+int checkBattery()
 {
     if (readVoltage(ADC, MAX_VOLTAGE, MAX_ADC_VALUE) <= 9.0f)
     {
         // critical voltage on batteries!
-        Serial.println("Shutting off all systems, going to deepsleep");
-        ESP.deepSleep(0); // Going into sleep mode until the system is restarted
+        // Serial.println("Shutting off all systems, going to deepsleep");
+        return 2;
+        // ESP.deepSleep(0); // Going into sleep mode until the system is restarted
     }
     else if (readVoltage(ADC, MAX_VOLTAGE, MAX_ADC_VALUE) <= 9.5f)
     {
         if (readVoltage(ADC, MAX_VOLTAGE, MAX_ADC_VALUE) <= 9.5f)
         {
             // warning
-            Serial.println("Battery low!");
+            // Serial.println("Battery low!");
+            return 1;
         }
     }
+    return 0;
 }
