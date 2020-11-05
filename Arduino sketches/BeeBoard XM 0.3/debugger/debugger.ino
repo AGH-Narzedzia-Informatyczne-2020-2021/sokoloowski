5537377e5ace6f1f43c917e09de26480842794b9
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-11-05 16:08:30 +0100

    Remote debugger sketch

---
+++ Arduino sketches/BeeBoard XM 0.3/debugger/debugger.ino
@@ -0,0 +1,20 @@
+/**
+ * @file debugger.ino
+ * @author Piotr Sokołowski (psokolowski@student.agh.edu.pl)
+ * @brief Recieve debug informations using ESP-NOW protocol
+ * @version 0.1
+ * @date 2020-11-05
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+void setup()
+{
+    // code...
+}
+
+void loop()
+{{
+    // code...
+}}
