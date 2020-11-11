e5943d48dd0765c037f6e987a91cd8ca5c3d60d0
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-11-11 19:39:25 +0100

    Reading ADC value

--- Arduino sketches/BeeBoard XM 0.3/main/battery.ino
+++ Arduino sketches/BeeBoard XM 0.3/main/battery.ino
@@ -8,3 +8,17 @@
  * @copyright Copyright (c) 2020
  * 
  */
+
+// Analog pin on ESP8266
+#define ADC 0
+
+/**
+ * @brief Read raw ADC value on specified port
+ * @param port number of analog port
+ */
+int readADC()
+{
+    int analogValue = 0;
+    analogValue = analogRead(ADC);
+    return analogValue;
+}
