cfffc566c860c85375fb4973378bd492d0f79a12
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-11-13 13:21:19 +0100

    Initializing weight sensor

--- Arduino sketches/BeeBoard XM 0.3/main/measurements.ino
+++ Arduino sketches/BeeBoard XM 0.3/main/measurements.ino
@@ -13,8 +13,19 @@
 #include <HX711.h>           // https://github.com/bogde/HX711/archive/master.zip
 #include <DHT.h>             // https://github.com/markruys/arduino-DHT/archive/master.zip
 
+#define HX711_DT 12
+#define HX711_SCK 13
+#define CALIBRATION_FACTOR 21700
+
 int pressure,
     humidity;
 float weight,
     temperature_in,
     temperature_out;
+
+void initWeight()
+{
+    hx711.begin(HX711_DT, HX711_SCK);
+    hx711.set_scale(CALIBRATION_FACTOR);
+    hx711.tare();
+}
